0f9f16d2bce08fd27cc2dac0f257f26e4bbda678
Socheat Sok <socheatsok78@gmail.com>
2024-06-27 16:28:36 +0700

    Inherit original functionality from smee-client cli

--- src/smee-client.js
+++ src/smee-client.js
@@ -1,21 +1,32 @@
 import process from 'node:process'
 import SmeeClient from 'smee-client'
+import { program } from 'commander'
+import { version } from '../package.json'
 
 const SIGNALS = ['SIGINT', 'SIGTERM']
 
-function main() {
-    const source = process.env.SOURCE_URL
-    const target = process.env.TARGET_URL
-    if (!source || !target) {
-        console.error('SOURCE_URL and TARGET_URL must be provided, exiting...')
-        process.exit(1)
+program
+    .version(version, '-v, --version')
+    .usage('[options]')
+    .option('-u, --url <url>', 'URL of the webhook proxy service. Default: https://smee.io/new')
+    .option('-t, --target <target>', 'Full URL (including protocol and path) of the target service the events will forwarded to. Default: http://127.0.0.1:PORT/PATH')
+    .option('-p, --port <n>', 'Local HTTP server port', process.env.PORT || 3000)
+    .option('-P, --path <path>', 'URL path to post proxied requests to`', '/')
+    .parse(process.argv)
+
+const opts = program.opts()
+
+const {
+    target = `http://127.0.0.1:${opts.port}${opts.path}`
+} = opts
+
+async function setup() {
+    let source = opts.url
+    if (!source) {
+        source = await SmeeClient.createChannel()
     }
-    const smee = new SmeeClient({
-        source: source,
-        target: target,
-        logger: console,
-    })
-    const events = smee.start()
+    const client = new SmeeClient({ source, target })
+    const events = client.start()
     SIGNALS.forEach(signal => {
         process.on(signal, () => {
             console.log(`Received ${signal} signal, exiting...`);
@@ -25,4 +36,4 @@ function main() {
     })
 }
 
-main();
+setup()
