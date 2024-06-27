a8adb63a9b259ceddb8b6c5cc63a408d89e8fcc6
Socheat Sok <socheatsok78@gmail.com>
2024-06-27 16:18:00 +0700

    Update src/server.js

--- src/server.js
+++ src/server.js
@@ -6,23 +6,16 @@ const SIGNALS = ['SIGINT', 'SIGTERM']
 function main() {
     const source = process.env.SOURCE_URL
     const target = process.env.TARGET_URL
-
     if (!source || !target) {
         console.error('SOURCE_URL and TARGET_URL must be provided, exiting...')
         process.exit(1)
     }
-
-    // Your code here
     const smee = new SmeeClient({
         source: source,
         target: target,
         logger: console,
     })
-
-    // Start the webhook forwarder
     const events = smee.start()
-
-    // Handle signals
     SIGNALS.forEach(signal => {
         process.on(signal, () => {
             console.log(`Received ${signal} signal, exiting...`);
